baf880c40c0f4f6c9a622b0a082e0f458dd8821f
Ealu-daa <ealutokeikankan@gmail.com>
2026-01-01 11:14:27 +0900

    getdocsをimport

--- ScoreJunken.js
+++ ScoreJunken.js
@@ -18,7 +18,8 @@ import {
   collection,
   orderBy, 
   limit,
-  query
+  query,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
